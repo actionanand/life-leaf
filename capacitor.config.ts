713bd479c2421fe03d2d892d2f8d0a362a680c7d
AnandRaja-tech <83357858+AnandRaja-tech@users.noreply.github.com>
2026-08-24 15:33:10 +0000

    minor chnages

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,25 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.actionanand.lifeleaf.app',
-  appName: 'Life Leaf',
+  appId: 'io.ionic.starter',
+  appName: 'life-leaf',
   webDir: 'www',
-  server: { androidScheme: 'https' },
-  android: { backgroundColor: '#f4f6f0' },
-  plugins: {
-    LocalNotifications: {
-      smallIcon: 'ic_stat_life_leaf',
-      iconColor: '#28734d',
-    },
-    SplashScreen: {
-      launchShowDuration: 1_800,
-      backgroundColor: '#f4f6f0',
-      showSpinner: false,
-      androidScaleType: 'CENTER_INSIDE',
-      splashFullScreen: true,
-      splashImmersive: false,
-    },
-  },
 };
 
 export default config;
